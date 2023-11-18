27218e43b371c9c3988e5dd7829ebba302a08603
katatsumuri <maimaidance0620@gmail.com>
2023-11-18 14:59:24 +0900

    weather_csv.py

--- jma_weather_api.py
+++ jma_weather_api.py
@@ -4,11 +4,12 @@ import json
 
 # 気象庁データの取得
 jma_url = "https://www.jma.go.jp/bosai/forecast/data/forecast/016000.json"
+sapporo_amedas = "https://www.jma.go.jp/bosai/amedas/data/map/20220118131000.json"
 jma_json = requests.get(jma_url).json()
 
 # 取得したいデータを選ぶ
-jma_date = jma_json[0]["timeSeries"][0]["timeDefines"][0]
-jma_weather = jma_json[0]["timeSeries"][0]["areas"][0]["weathers"][0]
+sapporo_time = jma_json[0]["timeSeries"][0]["timeDefines"][0]
+sapporo_weather = jma_json[0]["timeSeries"][0]["areas"][0]["weathers"][0]
 jma_rainfall = jma_json["Feature"][0]["Property"]["WeatherList"]["Weather"][0]["Rainfall"]
 # 全角スペースの削除
 jma_weather = jma_weather.replace('　', '')
