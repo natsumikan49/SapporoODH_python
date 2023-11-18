9e2fa0ad7b33e672d9fbe98c6b14e9256fd3c25f
natsumikan49 <reality.chrosigu.glycin.96@gmail.com>
2023-11-18 11:56:44 +0900

    天気

--- jma_weather_api.py
+++ jma_weather_api.py
@@ -3,13 +3,13 @@ import requests
 import json
 
 # 気象庁データの取得
-jma_url = "https://www.jma.go.jp/bosai/forecast/data/forecast/013000.json"
+jma_url = "https://www.jma.go.jp/bosai/forecast/data/forecast/016000.json"
 jma_json = requests.get(jma_url).json()
 
 # 取得したいデータを選ぶ
 jma_date = jma_json[0]["timeSeries"][0]["timeDefines"][0]
 jma_weather = jma_json[0]["timeSeries"][0]["areas"][0]["weathers"][0]
-#jma_rainfall = jma_json["Feature"][0]["Property"]["WeatherList"]["Weather"][0]["Rainfall"]
+jma_rainfall = jma_json["Feature"][0]["Property"]["WeatherList"]["Weather"][0]["Rainfall"]
 # 全角スペースの削除
 jma_weather = jma_weather.replace('　', '')
 
