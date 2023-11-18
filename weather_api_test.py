3fe0fd0e1eee35347d42785355e6c139faef40c8
katatsumuri <maimaidance0620@gmail.com>
2023-11-18 14:44:47 +0900

    weather_csv.pyの追加(札幌の降雪量のデータを取得)

---
+++ weather_api_test.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import requests
+import json
+
+# 気象庁データの取得
+jma_url = "https://www.jma.go.jp/bosai/forecast/data/forecast/{エリアコード}.json"
+jma_json = requests.get(jma_url).json()
+
+# 取得したいデータを選ぶ
+jma_date = jma_json[0]["timeSeries"][0]["timeDefines"][0]
+jma_weather = jma_json[0]["timeSeries"][0]["areas"][0]["weathers"][0]
+jma_rainfall = jma_json["Feature"][0]["Property"]["WeatherList"]["Weather"][0]["Rainfall"]
+# 全角スペースの削除
+jma_weather = jma_weather.replace('　', '')
+
+print(jma_date)
+print(jma_weather)
+print(jma_rainfall)
