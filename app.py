597df374e22a4fa0e1c71f7e3f0d19a0f9f708c7
natsumikan49 <reality.chrosigu.glycin.96@gmail.com>
2023-11-19 14:13:36 +0900

    白石予測モデル完成

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask
 import snowfall_csv
 import weather_forecast
+import yosoku_shiroishi
 app = Flask(__name__)
+
 
 @app.route("/")
 def showFall():
     snowfall = snowfall_csv.snowfall_dict
     data = weather_forecast.data
+    data["地点"] = "白石区"
+    yosoku = weather_forecast.jma_temp * yosoku_shiroishi.coef_temp + weather_forecast.jma_rain * yosoku_shiroishi.coef_rain + yosoku_shiroishi.intercept
+    data["24時間降雪量 現在値(cm)"] = yosoku
+    if yosoku < 0:
+        yosoku = 0
     data_json = dict(**data, **snowfall)
     return data_json
 
 if __name__ == "__main__":
     app.run()
+    
+    
 
--- weather_forecast.py
+++ weather_forecast.py
@@ -9,7 +9,12 @@ jma_json = requests.get(jma_url).json()
 # 取得したいデータを選ぶ
 jma_date = jma_json[0]["timeSeries"][0]["timeDefines"][1]
 jma_weather = jma_json[0]["timeSeries"][0]["areas"][0]["weathers"][1]
+jma_temp = jma_json[0]["timeSeries"][2]["areas"][0]["temps"][2]
+jma_rainmin = jma_json[1]["precipAverage"]["areas"][0]["min"]
+jma_rainmax = jma_json[1]["precipAverage"]["areas"][0]["max"]
 
+jma_rain = (float(jma_rainmax) + float(jma_rainmin) )/ 2.0
+print(jma_rain)
 # 全角スペースの削除
 jma_weather = jma_weather.replace('　', '')
 
